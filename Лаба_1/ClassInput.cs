7dcf219718dc2c15c828517973d97e8ff179374d
Юхновец Владимир Рис-22-1б <116095858+HardMain@users.noreply.github.com>
2023-09-22 10:21:13 +0500

    Add files via upload

---
+++ Лаба_1/ClassInput.cs
@@ -0,0 +1,18 @@
+﻿namespace Лаба_1
+{
+    internal class ClassInput
+    {
+        static public void Input(out double res, string msg, string errorMsg)
+        {
+            string? enter;
+            do
+            {
+                Console.Write(msg);
+                enter = Console.ReadLine();
+                if (!double.TryParse(enter, out res))
+                    Console.Write(errorMsg);
+
+            } while (!double.TryParse(enter, out res));
+        }
+    }
+}
---
+++ Лаба_1/Exercise_2.cs
@@ -0,0 +1,26 @@
+﻿namespace Лаба_1
+{
+    internal class Exercise_2
+    {
+        static public void SolutionEx2()
+        {
+            Console.WriteLine("Exercise2\n");
+
+            double x, y;
+
+            string errorMsg = "\nОшибка! Переменная должна быть вещественного типа!\n\n";
+
+            Console.WriteLine("Введите координаты точки: ");
+
+            ClassInput.Input(out x, "x?", errorMsg);
+            ClassInput.Input(out y, "y?", errorMsg);
+
+            bool res = (x * x + y * y <= 1 && !(x>0 && y>0));
+
+            Console.Write($"\nПопадание точки в область: {res}\n");
+
+            Console.Write("\nНажмите любую клавишу для продолжения");
+            Console.ReadKey();
+        }
+    }
+}
